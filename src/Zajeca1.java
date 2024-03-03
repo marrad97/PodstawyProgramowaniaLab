1f8fbb8cf69255c41e14bd645a76c0777fe0092e
martyna.radz <mar.rad97@gmail.com>
2024-03-03 14:31:28 +0100

    Initial commit

---
+++ src/Zajeca1.java
@@ -0,0 +1,34 @@
+public class Zajeca1 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 20;
+
+        double sum = x + y;
+        double sub = x - y;
+        double multi = x * y;
+        double div = x / y;
+
+        System.out.println("zadanie pierwsze");
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+
+        System.out.println("zadanie drugie");
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum2 = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]
+                + numbers[8] + numbers[9];
+
+        System.out.println("Średnia wynosi: "+sum2/numbers.length);
+
+
+        System.out.println("zadanie trzecie");
+        if (sum % 2 == 0) {
+            System.out.println("Liczba jest parzysta");
+        } else {
+            System.out.println("Nieparzysta");
+        }
+    }
+
+}
